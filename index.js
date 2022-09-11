878abe54540f3b4a35caf1570fefde6541317792
datdang3007 <datdang3007@gmail.com>
2022-09-11 16:14:29 +0700

    test submit

---
+++ index.js
@@ -0,0 +1,59 @@
+const linkMongo = "mongodb+srv://test:2StfOSSvmit3AWD1@cluster0.9nou7nd.mongodb.net/4Fruits?retryWrites=true&w=majority"
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect(linkMongo,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phone": phone,
+        "password" : password
+    }
+
+    // db.collection('users').insertOne(data,(err,collection)=>{
+    //     if(err){
+    //         throw err;
+    //     }
+    //     console.log("Record Inserted Successfully");
+    // });
+
+    console.log(data);
+
+    return res.redirect('signup_success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
